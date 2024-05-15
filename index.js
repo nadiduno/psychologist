ad0b375f633fffa2c95e0d98cc6ade69bc4b0938
nadiduno <nadiduno@gmail.com>
2024-05-14 22:44:05 -0300

    Setting express

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.get("/homepage", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => console.log("Server running on port 3000"));
