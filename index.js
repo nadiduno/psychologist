00810c36df31ebc46c733e88c31b3a4d038efb53
nadiduno <nadiduno@gmail.com>
2024-05-16 10:50:01 -0300

    Config index routes and controller

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const routes = require("./routes/routes");
 
 const app = express();
 
@@ -8,8 +9,6 @@ const port = 3000;
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use(routes);
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
